79e01773a626f0d09be6c949dfa0b6641ec29047
hiian2890 <hiian2890@gmail.com>
2025-11-21 10:02:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app= Flask(__name__)
 
 def index():
     return(render_template ("index.html"))
-
+def main():
+    q=request.get("q")
+    print(q)
+    return(render_template("main.html"))
+    
 if __name__== "__main__":
     app.run()
